6f93929fc1d9a603c427ee048a950bd20cb5799c
Arkadiusz Kulewicz <a.kulewicz@gmail.com>
2020-05-30 20:10:50 +0200

    add logic to main.js

---
+++ main.js
@@ -0,0 +1,29 @@
+const form = document.querySelector('.quiz-form--js');
+const resultsWrapper = document.querySelector('.results-wrapper--js');
+const pointsBox = document.querySelector('.special--points');
+const percentBox = document.querySelector('.special--percent');
+
+const correctAnswers = ['A','B','A','B'];
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    let pointsCounter = 0;
+    const maxPoints = correctAnswers.length;
+    const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
+
+    userAnswers.forEach((answer, index) => {
+        if (answer === correctAnswers[index]) {
+            pointsCounter += 1;
+        }
+    })
+
+    pointsBox.textContent = pointsCounter;
+    percentBox.textContent = (pointsCounter/maxPoints)*100 + '%';
+    resultsWrapper.style.display = 'flex';
+
+
+
+
+})
+
